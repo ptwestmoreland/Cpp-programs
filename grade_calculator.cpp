c3dff4c0b9f28cc2c58621b671058973c8494860
ptwestmoreland <89505097+ptwestmoreland@users.noreply.github.com>
2022-06-06 11:29:33 -0500

    Add files via upload

--- grade_calculator.cpp
+++ grade_calculator.cpp
@@ -18,13 +18,14 @@ not .752 */
 #include <sstream>
 #include <limits>
 
+// ONLY MAKE CHANGES WHERE THERE IS A TODO
 
+// These using declarations let us refer to things more simply
+// e.g. instead of "std::cin" we can just write "cin"
 using std::cin, std::cout, std::endl;
 using std::string, std::getline;
 
-// function declarations before main, definitions are listed after
-
-// These methods are defined below the main function
+// These functions are defined below the main function
 
 // print instructions for inputting grades
 void print_instructions();
@@ -50,7 +51,7 @@ void get_category_and_score(const string& line,
 int main() {
     print_instructions();
 
-
+    // ONLY MAKE CHANGES WHERE THERE IS A TODO
 
    //declaring needed variables:
 
@@ -74,14 +75,14 @@ int main() {
     double total_read_score = 0.0;
     int num_reading_scores = 0;
 
-    //engagement score variables, this is akin to a participation grade
+    //engagement score variables
 
     double total_engage_score = 0.0;
-    int num_engage_scores = 0;
+    int num_engage_scores = 0 ;
 
 
      string line;
-    // read one line from standard input
+    // read one line from standard input (discards the ending newline character)
     getline(cin, line);
     // read lines until an empty line is read
     while (!line.empty()) { //begin processing loop
@@ -101,10 +102,11 @@ int main() {
             homework_score_total = score + homework_score_total; //keeps track of total hw grade
             number_of_homework_scores++; //tracks number of hw entries throughout loop
         } else if (category == "lw") {
-            if( (score - 1.0) < 0.01 ){ //== comparison not good for doubles
-               total_lw_score = total_lw_score + 100; //give 100 to total score for each complete lw
+            if((score == 1.0) < 0.00001){ //inner if statement
+               total_lw_score = total_lw_score + 100.0; //give 100 to total score for each complete lw
                number_of_lw++; //tracks lw
-             }
+
+            }
             else{
                 total_lw_score = total_lw_score + 0; //adds 0 for each incomplete
                 number_of_lw++;
@@ -117,32 +119,32 @@ int main() {
             num_engage_scores++;
         } else {
             cout << "ignored invalid input" << endl;
-        } //end if else if block
+        }
 
         // get the next line from standard input
         getline(cin, line);
     } //end file processing loop
 
 
-    /* averages are computed as total divided by the number of entries
+    /*averages are computed as total divided by the number of entries
     if a student has no entries for some category, then
     we will have a divide by zero error. So each tracking
     variable that keeps up with the number of entries, must be
     set to not equal 0. */
 
-    if(num_engage_scores == 0){
+    if(num_engage_scores == 0.0){
         num_engage_scores++; //sets it equal to 1, dividing by 1 will not change the value
     }
-    if(num_reading_scores == 0){
+    if(num_reading_scores == 0.0){
         num_reading_scores++;
     }
-    if(number_of_exams == 0){
+    if(number_of_exams == 0.0){
         number_of_exams++;
     }
-    if(number_of_lw == 0){
+    if(number_of_lw == 0.0){
         number_of_lw++;
     }
-    if(number_of_homework_scores == 0){
+    if(number_of_homework_scores == 0.0){
         number_of_homework_scores++;
     }
 
@@ -153,50 +155,42 @@ int main() {
     double lw_average = 0.0;
     double reading = 0.0;
     double engagement = 0.0;
-    //exam_av = (the total exam 1, 2 sum + final) / 3 tests
-    //reading and engage have 15% added on up to 100
-    exam_average = (exam_score_total + final_exam_score) / 3; //always 3
+
+    exam_average = (exam_score_total + final_exam_score) / 3.0;
     hw_average = homework_score_total / number_of_homework_scores;
     lw_average = total_lw_score / number_of_lw;
-    reading = (total_read_score / num_reading_scores) + 15.0;
-    engagement = (total_engage_score / num_engage_scores) + 15.0;
-
-    //method commented out was used to add the 15% to engage and read
-    //it was dropped for more efficient method
+    reading = total_read_score / num_reading_scores;
+    engagement = total_engage_score / num_engage_scores;
 
     //reading and engagement have 15 points added on to them up to 100
     //if statements to append needed points on to two categories:
-  /*  if(reading <= 85){
-        reading += 15; //will add 15 points to any score less than or equal to 85
+
+    /* the simpler way to do this is to add 15 to each average and set
+    anything over 100 = to 100, but I wanted to try this method */
+
+    if(reading <= 85.0){
+        reading += 15.0; //will add 15 points to any score less than or equal to 85
     }
-    if(reading > 85 && reading < 100){
-        double point_tracker = 0;
-        point_tracker = 100 - reading; //find points that will make the score 100
+    if(reading > 85.0 && reading < 100.0){
+        double point_tracker = 0.0;
+        point_tracker = 100.0 - reading; //find points that will make the score 100
         reading = reading + point_tracker; //appends them to score to make it 100
     }
-    if(reading >= 100){ //in case of some error where reading is greater than 100
-        reading = 100;
+    if(reading >= 100.0){ //in case of some error where reading is greater than 100
+        reading = 100.0;
     }
     //repeat for engagement
-    if(engagement <= 85){
-        engagement += 15;
+    if(engagement <= 85.0){
+        engagement += 15.0;
     }
-    if(engagement > 85 && engagement < 100){
+    if(engagement > 85.0 && engagement < 100.0){
         double point_tracker2 = 0;
-        point_tracker2 = 100 - engagement;
+        point_tracker2 = 100.0 - engagement;
         engagement = engagement + point_tracker2;
     }
-    if(engagement >= 100){
-        engagement = 100;
-    } //end if statements to add up to 15 points to two categories */
-
-    if(engagement > 100.0){
-      enagement = 100.0; //sets engage to 100% if adding 15 caused it to go above that
-    }
-    if(reading > 100.0) {
-      reading = 100.0;
-
-    }
+    if(engagement >= 100.0){
+        engagement = 100.0;
+    } //end if statements to add up to 15 points to two categories
 
     /*if the final is greater than the total exam average then it is used in the computation
     of the final grade */
@@ -222,30 +216,30 @@ int main() {
     char final_letter_grade = 'X'; //holds letter grade
 
    //grades classified as A-B-C-D-F in certain ranges of points earned
-    if(weighted_total >= 90){ //if total points is greater than 90 user gets an A
+    if(weighted_total >= 90.0){ //if total points is greater than 90 user gets an A
         final_letter_grade = 'A';
     }
-    if(weighted_total >= 80 && weighted_total < 90){
+    if(weighted_total >= 80.0 && weighted_total < 90.0){
          final_letter_grade = 'B'; //if total points is in between 80 - 89.9 user gets B
      }
-    if(weighted_total >= 70 && weighted_total < 80){
+    if(weighted_total >= 70.0 && weighted_total < 80.0){
          final_letter_grade = 'C';
      }
 
-    if(weighted_total >= 60 && weighted_total < 70){
+    if(weighted_total >= 60.0 && weighted_total < 70.0){
         final_letter_grade = 'D';
     }
-    if(weighted_total < 60){
+    if(weighted_total < 60.0){
         final_letter_grade = 'F';
     }
 
     print_results(
         exam_average, hw_average, lw_average, reading, engagement,
         weighted_total, final_letter_grade);
-
- return 0;
 }
 
+// These methods are already implemented for you
+// You should not need to modify them
 
 void print_instructions() {
     cout << "enter grades as <category> <score>" << endl;
