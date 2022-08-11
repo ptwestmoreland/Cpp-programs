802e80d52e652bbab4f4d9759a4023978c0225d5
ptwestmoreland <89505097+ptwestmoreland@users.noreply.github.com>
2022-08-11 00:02:07 -0500

    Add files via upload
    Check the read me or source code for a description.

---
+++ Caesar_encode_class.cpp
@@ -0,0 +1,84 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* this class allows a user to send in an input word and have it encoded via the Caesar cipher
+
+This exercise need not be done with a class, I am just trying to get some practice with object oriented programming
+
+ Im using a hand set shift key of 4, but in practice the key should be user set and that is easily done  */
+class encoded_word{
+    
+    private:
+    string userInput;
+    string encodedOutput;
+    
+    public:
+    encoded_word();
+    string encode_word(string input);
+    
+    
+};
+// public member function definitions 
+
+encoded_word::encoded_word(){
+    userInput = ""; // constructor initialization of object 
+    encodedOutput = "";
+}
+string encoded_word::encode_word(string input){
+    // this public function recieves an input from main and alters it with the Caesar cipher method 
+    encodedOutput = input; // set encoded output to what ever is sent in from main 
+    // shift each alphabetic character down by 4 units
+    
+    // convert string to all lower case and make sure the input string is alphabetic  
+    
+    for(unsigned int i = 0; i < input.size(); ++i){
+        if(isalpha(encodedOutput.at(i))){
+            
+            
+              encodedOutput.at(i) = tolower(encodedOutput.at(i)); 
+                
+         }
+        else{
+            throw std::invalid_argument("String must have only alphabetic characters.");
+        }
+    }
+    for( unsigned int i = 0; i < input.size(); ++i){
+        // string has been checked for alphabetics characters at this point 
+        if(encodedOutput.at(i) <= 'v') { // if the character is "greater" than z, then we get a non alphabetic letter when shifting
+           encodedOutput.at(i) = encodedOutput.at(i) + 4; // shift 4 letters down 
+    } // end if for letters that can be shifted by 4 without a wraparound 
+        else{     // some letter is greater than v 
+            // say a letter is z, it should be shifted from z -> d by wrapping back around 
+            encodedOutput.at(i) = encodedOutput.at(i) - 'w' + 'a'; // takes care of shift to non alphabetic characters with key of 4
+        }
+    
+ } // end for loop 
+ 
+ return encodedOutput; 
+
+} // end encode_word function 
+
+
+int main()
+{
+    // testing 
+    string userIn = "";  
+    cin >> userIn;
+    encoded_word practice_word;
+     
+     string x = practice_word.encode_word(userIn);
+     cout << x << endl;
+     
+     
+
+    return 0;
+}
