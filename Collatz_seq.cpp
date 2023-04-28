9df4669c2873d5b43c170de95b3c3b9f449824df
ptwestmoreland <89505097+ptwestmoreland@users.noreply.github.com>
2023-04-27 19:49:05 -0500

    Update Collatz_seq.cpp

--- Collatz_seq.cpp
+++ Collatz_seq.cpp
@@ -1,14 +1,8 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
 int collatz(int n){ // define collatz, 1 integer parameter and an integer return value
     
@@ -32,13 +26,19 @@ int main()
     cout << "Enter a number: ";
     int n = 0;
     cin >> n;
+   // make sure that n is greater than 0, not using try - throw
+    while(n <= 0){
+        cout << "Please enter a positive integer value: ";
+        cin >> n;
+    }
+    // n should be checked to be an integer/ non floating point value because floats break the program
     cout << "The collatz sequence for " << n << " is:" << endl;
     cout << n << " "; // printing of starting point is done once as opposed to each time the function is called
     int k = collatz(n); // initialize k with first term in sequence
     
     while(k != 1){
         k = collatz(k); // perform the algorithm on each value of k and then compare loop condition
-    }
+    } // end while
    
 
     return 0;
